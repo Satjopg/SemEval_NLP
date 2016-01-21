8d139d0424400197f9fc200a3b362b77da7c56e5
Satoru Murakami <Satoru@SatsMac.local>
2016-01-22 02:23:37 +0900

    delete extra features

--- comword.py
+++ comword.py
@@ -1,12 +1,8 @@
 ## source ./usr/test/bin/activate
 ## coding: UTF-8
 
-from gensim.models import word2vec
 import re
-import math
 from lcs import *
-
-model = word2vec.Word2Vec.load("example.model")
 
 # 共通単語数(StopWord)
 # ジャッカード係数で類似度算出
@@ -15,25 +11,6 @@ def get_coms(a, b):
 	sent2 = set(word for word in b.split(' ') if not word in get_stopword())
 	return (len(sent1.intersection(sent2))*1.0)/(len(sent1.union(sent2))*1.0)
 	
-# 共通単語数(StopWord, w2v)
-def get_comsw(a,b):
-	sent1 = set(word for word in a.split(' ') if not(word in get_stopword()))
-	sent2 = set(word for word in b.split(' ') if not(word in get_stopword()))
-	cnt = 0
-	for w1 in sent1:
-		for w2 in sent2:
-			try:
-				similarity = round(model.similarity(w1, w2), 1)
-			except KeyError:
-				if w1 == w2:
-					similarity = 1.0
-				else:
-					similarity = 0
-			finally:
-				if similarity >= 0.8:
-					cnt += similarity
-	return (cnt*1.0)/(len(sent1.union(sent2))*1.0)
-
 # main文
 if __name__ == '__main__':
 	
@@ -50,8 +27,6 @@ if __name__ == '__main__':
 		line2 = re.sub('\\(|\\)|:|;|,|\.|\\n|\\r|\'s|#', '', line1)
 		line = re.sub('\-', ' ', line2)
 		sentences = line.split('\t')
-		sim = get_comsw(sentences[0], sentences[1])
-		similarity = round(sim, 1)
-		fw.write(str(similarity))
-		fw.write('\n')
+		sim = get_coms(sentences[0], sentences[1])
+		fw.write(str(round(sim, 1))+'\n')
 	fw.close()
