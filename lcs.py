f5cd9d0dcd138f3a85cb5149bbc2f00c6a4f6a85
Satoru Murakami <Satoru@SatsMac.local>
2016-01-21 15:29:58 +0900

    edit origin lcs

--- lcs.py
+++ lcs.py
@@ -1,37 +1,20 @@
-## source ./usr/test/bin/activate
 ## coding: UTF-8
 
-from gensim.models import word2vec
 import re
 import math
 
-model = word2vec.Word2Vec.load("example.model")
-
 # a,b２つの文のLCS長を求める
 def get_lcs(a, b):
-	stop = get_stopword()
-	cnt = 0
+#	stop = get_stopword()
 	sent1 = a.split(' ')
 	sent2 = b.split(' ')
 	LCS = [[0 for i in range(len(sent2) + 1)] for j in range(len(sent1) + 1)] 
 	for x in range(len(sent1)):
-		if sent1[x] in stop:
-			continue;
 		for y in range(len(sent2)):
-			if sent2[y] in stop:
-				continue;
-			try:
-				similarity = round(model.similarity(sent1[x], sent2[y]), 1)
-			except KeyError:
-				if sent1[x] == sent2[y]:
-					similarity = 1.0
-				else:
-					similarity = 0
-			finally:
-				if similarity >= 0.7:
-					LCS[x+1][y+1] = max(LCS[x][y+1], LCS[x+1][y], LCS[x][y]+similarity)
-				else:
-					LCS[x+1][y+1] = max(LCS[x][y+1], LCS[x+1][y], LCS[x][y])
+			if sent1[x] == sent2[y]:
+				LCS[x+1][y+1] = max(LCS[x][y+1], LCS[x+1][y], LCS[x][y]+1)
+			else:
+				LCS[x+1][y+1] = max(LCS[x][y+1], LCS[x+1][y], LCS[x][y])
 	return LCS[len(sent1)][len(sent2)]
 
 def get_stopword():
@@ -44,34 +27,18 @@ def get_stopword():
 		stop.add(word)
 	return stop
 
-def get_len(sentence):
-	stop = get_stopword()
-	count = 0
-	for word in sentence:
-		if word in stop:
-			continue;
-		count += 1
-	return count
-
 # main文
 if __name__ == '__main__':
     fr = open('test.txt')
     lines = fr.readlines() 
-    # 1行毎にファイル終端まで全て読む(改行文字も含まれる)
     fr.close()
-    # lines: リスト。要素は1行の文字列データ
     	
-    # 書き込み用
     fw = open('text.txt', 'w')
-    
     for line1 in lines:
         line2 = re.sub('\\(|\\)|:|;|,|\.|\\n|\\r|\'s', '', line1)
         line = re.sub('\-', ' ', line2)
         sentences = line.split('\t')
         lcs = get_lcs(sentences[0].lower(), sentences[1].lower())
-        simori = (lcs * 2.0) / (min(len(sentences[0].split(' ')), len(sentences[1].split(' '))*1.0))
-        #simori = (lcs * 2.0) / (max(get_len(sentences[0].split(' ')), get_len(sentences[1].split(' '))*1.0))
-        sim = round(simori, 1)
-        fw.write(str(sim))
-        fw.write("\n")
+        sim = (lcs * 1.0) / (max(len(sentences[0].split(' ')), len(sentences[1].split(' '))*1.0))
+        fw.write(str(round(sim,1))+"\n")
     fw.close()
